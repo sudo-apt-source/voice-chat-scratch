01d2f7815846ffbcc57dd8fcefc60ea2c3e14420
sudo-apt-source <yousifamar116@gmail.com>
2026-03-09 20:03:27 +0400

    Refactor socket event handling and player management

--- server.js
+++ server.js
@@ -1,28 +1,46 @@
 const { Server } = require("socket.io");
 
-const io = new Server(3000, {
-  cors: {
-    origin: "*"
-  }
+const io = new Server(3000,{
+  cors:{origin:"*"}
 });
+
+let players = {};
 
 io.on("connection", socket => {
 
-  socket.on("join-room", room => {
+  socket.on("join", data => {
 
-    socket.join(room);
+    players[socket.id] = {
+      room:data.room,
+      team:data.team,
+      x:0,
+      y:0
+    };
 
-    socket.to(room).emit("user-connected", socket.id);
+    socket.join(data.room);
+
+    socket.to(data.room).emit("player-join",socket.id);
 
   });
 
-  socket.on("signal", ({ target, data }) => {
+  socket.on("position", pos => {
 
-    io.to(target).emit("signal", {
-      sender: socket.id,
-      data: data
+    if(players[socket.id]){
+      players[socket.id].x = pos.x;
+      players[socket.id].y = pos.y;
+    }
+
+  });
+
+  socket.on("signal", data=>{
+    io.to(data.target).emit("signal",{
+      sender:socket.id,
+      data:data.data
     });
+  });
 
+  socket.on("disconnect",()=>{
+    delete players[socket.id];
   });
 
 });
